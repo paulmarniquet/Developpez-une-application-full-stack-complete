a649bdafd58e76738097d33b551b3a529e3287b8
paulmarniquet <paul.marniquet@epitech.eu>
2023-12-11 12:14:28 +0100

    Created Header and Articles components

---
+++ front/src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+    showNavbar: boolean = true;
+
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        const excludedPages: string[] = ['login', 'register'];
+        this.activatedRoute.url.subscribe(() => {
+            const currentUrl = this.router.url;
+            this.showNavbar = !excludedPages.some(page => currentUrl.includes(page));
+        });
+    }
+}
---
+++ front/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}",
+    ],
+    theme: {
+    extend: {
+        colors: {
+            'bluefont': '#6C5CCF',
+        },
+    },
+  },
+  plugins: [],
+}
+

41383579c027f6f410ae610cd7a7132c8ddfe0e9
paulmarniquet <paul.marniquet@epitech.eu>
2023-12-20 18:47:36 +0100

    Create post

--- back/src/main/java/com/openclassrooms/mddapi/service/ArticleService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/ArticleService.java
@@ -1,16 +1,27 @@
 package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.entity.Article;
+import com.openclassrooms.mddapi.entity.Topic;
+import com.openclassrooms.mddapi.entity.User;
 import com.openclassrooms.mddapi.repository.ArticleRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Data
 @Service
 public class ArticleService {
+
+    @Autowired
+    private final TopicService topicService;
+
+    @Autowired
+    private final UserService userService;
 
     @Autowired
     private final ArticleRepository articleRepository;
@@ -25,6 +36,23 @@ public class ArticleService {
 
     public Optional<Article> saveArticle(Article article) {
         return Optional.of(articleRepository.save(article));
+    }
+
+    public Iterable<Article> getFeed(Long userId) {
+        User user = userService.getUser(userId).get();
+        List<Number> topicIds = new ArrayList<>();
+        for (Topic topic : user.getTopics()) {
+            topicIds.add(topic.getId());
+        }
+
+        List<Article> userArticles = new ArrayList<>();
+        Iterable<Article> articles = articleRepository.findAll();
+        for (Article article : articles) {
+            if (topicIds.contains(article.getTopic().getId())) {
+                userArticles.add(article);
+            }
+        }
+        return userArticles;
     }
 
 }
